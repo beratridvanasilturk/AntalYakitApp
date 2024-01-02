63e3aa96a99fc057f7661026411775034bb8d03f
beratridvanasilturk <br.asltrk@gmail.com>
2024-01-03 01:36:31 +0300

    Added ResultShowRestaurant View & Fetching Restaurant Name

--- AntalYakit/screens/ResultShowRestaurant.js
+++ AntalYakit/screens/ResultShowRestaurant.js
@@ -1,10 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import yelp from "/Users/asilturk/Developer/ReactNativeProjects/AntalYakitApp/AntalYakit/screens/api/yelp.js";
 
-export default function ResultShowRestaurant() {
+export default function ResultShowRestaurant({route} ) {
+  const [restaurant, setRestaurant] = useState(null);
+  const id = route.params.id;
+  const getRestaurantDetail = async (id) => { 
+    // istekten donen cevabi response'a atiyoruz.
+    // await kullanabilmek icin async yapmamiz gerekiyor, Swift'deki async ile ayni. =) main thread'i kitlememek icin await kullanilir. =)
+    // await'in amaci cevap gelene kadar beklemesini saglamak, Swift'deki await ile ayni. =) 
+    // baseURL sonrasinda / ekleyerek id'leri cekiyoruz 
+    const response = await yelp.get(`/${id}`); 
+    setRestaurant(response.data);
+  };
+
+  // useEffect() : UiKit'deki viewDidLoad() gibi calistirmak icin virgulden sonra bos array verirsek sadece bir kere calisir.
+  // useEffect() aslinda SwiftUI'deki onAppear() 'in aynisidir. didLoad'a cevirmek icin virgulden sonra [] sarttir.
+  useEffect(() => {
+    getRestaurantDetail(id);
+  }, []);
+  
+  // data gelene kadar hata vermemesi icin restaurant check ediyoruz.
+  if (!restaurant) {
+    return null;  
+  }
+
   return (
     <View>
-      
+      <Text>{restaurant.name}</Text>
     </View>
   )
 }
