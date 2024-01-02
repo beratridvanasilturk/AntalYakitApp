7dfbab8a58734a2edd12eaa1accb5a6975fec65c
beratridvanasilturk <br.asltrk@gmail.com>
2024-01-02 21:04:32 +0300

    Added Axios & Request Js File for Base Url

---
+++ AntalYakit/screens/api/request.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://api.yelp.com/v3/businesses",
+    headers: {
+        Authorization : 'Bearer 9ar_tRZmuJjYFGetMvdWTmjOSj4Cjn4l_HHFn9LNu36GDRSw0REJu30L6BJcOAjkrSoBBs9NUftmcDnOQDe5m7Ynf8v2kIvK8WaUpAFjwoGoHPxzZYJU0dVFe6KTZXYx'
+    },
+    responseType: "json"
+});
